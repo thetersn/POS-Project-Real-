f610dd043db722d8b9ea8cd501d1edad88aa9e0c
Uttawoot <31436862+thetersn@users.noreply.github.com>
2023-01-10 20:58:10 +0700

    Update _contain_user_01.php

--- pages/_contain_user_01.php
+++ pages/_contain_user_01.php
@@ -1,168 +1,237 @@
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<div class="row">
-  <div class="col-xl-12 col-md-12 mb-2">
-    <h1 class="h3 mb-2 text-dark font-weight-bold">ก๋วยเตี๋ยว</h1>
-    <hr>
-  </div>
-</div>
-
-
-<article class="row">
-  <div class="col-xl-6 col-md-6 mb-4">
-    <div class="card border-left-danger shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-xl-12 col-md-12 mb-4 mr-10">
-            <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
-          </div>
-
-          <div class="col-auto">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-              <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
-            </div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
-          </div>
-
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-xl-6 col-md-6 mb-4">
-  <a href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-    <div class="card border-left-danger shadow h-100 py-2" >
-      <div class="card-body">
-
-        <div class="row no-gutters align-items-center">
-          <div class="col-xl-12 col-md-12 mb-4 mr-10">
-            <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
-          </div>
-
-          <div class="col-auto">
-     
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-              <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
-            </div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
-          </div>
-
+        <div class="modal-body">
+          ...
         </div>
-      </div>
-    </div>
-  
-  </div>
-  
-
-
-
-
-  <div class="col-xl-6 col-md-6 mb-4">
-    <div class="card border-left-danger shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-xl-12 col-md-12 mb-4 mr-10">
-            <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
-          </div>
-
-          <div class="col-auto">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-              <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
-            </div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
-          </div>
-
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
   </div>
 
 
-  <div class="col-xl-6 col-md-6 mb-4">
-    <div class="card border-left-danger shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-xl-12 col-md-12 mb-4 mr-10">
-            <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
-          </div>
-
-          <div class="col-auto">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-              <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
-            </div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
-          </div>
-
-        </div>
-      </div>
+  <div class="row">
+    <div class="col-xl-12 col-md-12 mb-2">
+      <h1 class="h3 mb-2 text-dark font-weight-bold">ก๋วยเตี๋ยว</h1>
+      <hr>
     </div>
   </div>
 
 
-  <div class="col-xl-6 col-md-6 mb-4">
-    <div class="card border-left-danger shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col-xl-12 col-md-12 mb-4 mr-10">
-            <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
-          </div>
-
-          <div class="col-auto">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-              <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
+  <article class="row">
+    <div class="col-xl-6 col-md-6 mb-4">
+      <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col-xl-12 col-md-12 mb-4 mr-10">
+              <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
             </div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
-          </div>
 
+            <div class="col-auto">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
+              </div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
+    <div class="col-xl-6 col-md-6 mb-4">
+      <a href="#" data-toggle="modal" data-target="#logoutModal">
 
+        <div class="card border-left-danger shadow h-100 py-2">
+          <div class="card-body">
 
+            <div class="row no-gutters align-items-center">
+              <div class="col-xl-12 col-md-12 mb-4 mr-10">
+                <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
+              </div>
 
-</article>
+              <div class="col-auto">
 
-
-      <!-- Logout Modal-->
-      <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                  <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
-                </div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
+              </div>
+
+
+
             </div>
+          </div>
         </div>
+      </a>
     </div>
+
+
+
+
+
+    <div class="col-xl-6 col-md-6 mb-4">
+      <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col-xl-12 col-md-12 mb-4 mr-10">
+              <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
+            </div>
+
+            <div class="col-auto">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
+              </div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-xl-6 col-md-6 mb-4">
+      <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col-xl-12 col-md-12 mb-4 mr-10">
+              <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
+            </div>
+
+            <div class="col-auto">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
+              </div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-xl-6 col-md-6 mb-4">
+      <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col-xl-12 col-md-12 mb-4 mr-10">
+              <img class="rounded  img-fluid" src="https://www.sgethai.com/wp-content/uploads/2021/10/211014-Content-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%B9%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B3%E0%B8%82%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%AA%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%94%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%84%E0%B8%A1%E0%B9%88%E0%B8%95%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%9B%E0%B8%A3%E0%B8%B8%E0%B8%8703-1200x670.jpg" alt="">
+            </div>
+
+            <div class="col-auto">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                <h4 class="text-danger font-weight-bold">ก๋วยเตียวน้ำตก</h4>
+              </div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">$40</div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+  </article>
+
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h5 class="modal-title">เลือกเส้น</h5>
+          <div class="form-group col mt-3">
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+              <label class="form-check-label" for="exampleRadios1">
+                เส้นเล็ก
+              </label>
+            </div>
+
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+              <label class="form-check-label" for="exampleRadios2">
+                เส้นใหญ่
+              </label>
+            </div>
+
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled>
+              <label class="form-check-label" for="exampleRadios3">
+                หมี่เหลือง
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" disabled>
+              <label class="form-check-label" for="exampleRadios2">
+                หมี่ขาว
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" disabled>
+              <label class="form-check-label" for="exampleRadios2">
+                วุ้นเส้น
+              </label>
+            </div>
+          </div>
+
+
+
+          <h5 class="modal-title">เลือกเนื้อสัตว์</h5>
+          <div class="form-group col mt-3">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+              <label class="form-check-label" for="defaultCheck1">
+                ไก่
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2" disabled>
+              <label class="form-check-label" for="defaultCheck2">
+                หมู
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2" disabled>
+              <label class="form-check-label" for="defaultCheck2">
+                ทะเล
+              </label>
+            </div>
+
+          </div>
+
+
+          <div class="text-center mt-5">
+            <a class="btn btn-primary btn-block" href="logout.php">สั่งอาหาร</a>
+          </div>
+
+
+        </div>
+        <!-- <div class="modal-footer">
+          
+           <a class="btn btn-primary" href="logout.php">สั่งอาหาร</a> 
+           
+        </div> -->
+      </div>
+    </div>
+  </div>
